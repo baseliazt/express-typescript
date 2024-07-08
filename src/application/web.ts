73fe9d3b44a93e01b6eb9f020f161da5e84eccc7
juniantoPalmCode <junianto.palmco@aaico.com>
2024-07-09 00:47:10 +0700

    feat: registration api

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { publicRouter } from "../route";
+import { errorMiddleware } from "../middleware";
 
 export const web = express();
 web.use(express.json());
+web.use(publicRouter);
+web.use(errorMiddleware);
