a3ef37c3a48294127f49d39c903698d1a5ad6952
milyasbpa <basilyas.2015@gmail.com>
2024-07-09 16:29:16 +0700

    test: valid user

--- src/test/user.test.ts
+++ src/test/user.test.ts
@@ -1,8 +1,12 @@
 import supertest from "supertest";
 import { web } from "../application/web";
 import { logger } from "../application/logging";
+import { UserTest } from "./test.util";
 
 describe("POST /api/users", () => {
+  afterEach(async () => {
+    await UserTest.delete();
+  });
   it("should be reject register new user if request is invalid", async () => {
     const response = await supertest(web).post("/api/users").send({
       username: "",
@@ -13,5 +17,18 @@ describe("POST /api/users", () => {
     logger.debug(response.body);
     expect(response.status).toBe(400);
     expect(response.body.errors).toBeDefined();
+  });
+
+  it("should register new user if request is valid", async () => {
+    const response = await supertest(web).post("/api/users").send({
+      username: "test",
+      name: "test",
+      password: "test",
+    });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("test");
+    expect(response.body.data.name).toBe("test");
   });
 });
