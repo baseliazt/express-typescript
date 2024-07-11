6b57c10b0801d9e522c45e82d3961b4d210df2dd
milyasbpa <basilyas.2015@gmail.com>
2024-07-11 20:28:57 +0700

    fix: auth middleware give return

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { prismaClient } from "../application/database";
 import { UserRequest } from "../type";
 import { ResponseError } from "../error";
+import { logger } from "../application/logging";
 
 export const authMiddleware = async (
   req: UserRequest,
@@ -16,12 +17,19 @@ export const authMiddleware = async (
         token: token,
       },
     });
+    logger.debug("test", user);
 
     if (user) {
       req.user = user;
       next();
+      return;
+    } else {
+      res.status(401).json({
+        errors: "Unauthorized",
+      });
     }
-
-    throw new ResponseError(401, "Unauthorized");
   }
+  res.status(401).json({
+    errors: "Unauthorized",
+  });
 };
